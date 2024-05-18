15289cea82217bb4420f8ac5d24fca19ab67dbb8
M-Mortes <139556640+M-Mortes@users.noreply.github.com>
2024-05-19 01:51:14 +0200

    Fixing stuff

--- obj/Debug/net8.0/ConsoleAdventure.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleAdventure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleAdventure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a7b2563744d7d0dfb5d20d5c159305e42091f17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a601d3e35d50bc53a60272ca7d5b0db07867ddc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleAdventure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleAdventure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
